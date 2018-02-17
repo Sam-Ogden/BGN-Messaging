c9101faa157783250c063af0dc593835269160a6
Samuel Ogden <sam.george.ogden@gmail.com>
2018-02-17 12:46:39 +0000

    merged dev

---
+++ README.md
@@ -0,0 +1,5 @@
+# BGN Messaging 
+
+Documentation: apidoc/index.html
+
+No UI is provided to test this system.  
---
+++ apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "BGN Messaging API Docs",  "version": "0.1.0",  "description": "BGN Messaging API Docs",  "apidoc": "0.3.0",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "generator": {    "name": "apidoc",    "time": "2018-02-16T23:26:26.456Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
